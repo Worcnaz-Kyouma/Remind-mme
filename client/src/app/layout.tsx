0023d0abc13d5f6d26201b805190a8fe9e133ddd
PradoNicolas <nicolasprado.aquicob@gmail.com>
2023-08-29 17:53:16 -0300

    title patch

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Remmind-mme',
+  title: 'Remind-mme',
   description: 'Keep calm, we gonna remind you!',
   icons:{
     icon: '/favicon3.png'
